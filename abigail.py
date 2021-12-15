9d372c33d532015d35bbeec93b3bc088150b290c
sifisega <hallis1002@gmai.com>
2021-12-15 19:35:13 +1300

    Added missing code

--- abigail.py
+++ abigail.py
@@ -3,12 +3,13 @@ EMPLOYEES = {
     'Allan':[],
     'Fraser':[]
 }
-JOBS = []
+JOBS = {'0':[], }
 
 class Job():
     # defines a job that is a collection of shifts
-    def __init__(self):
+    def __init__(self, key):
         self.shifts = []
+        JOBS[key] = self
 
     def add_shift(self, shift):
         self.shifts.append(shift)
@@ -16,7 +17,7 @@ class Job():
 
 class Shift():
     # defines a period of time an employee worked towards a specific job
-    def __init__(self, start_time = 0, end_time = 0, date = 0, job = False, employee = ''):
+    def __init__(self, start_time=0, end_time=0, date=0, job=False, employee=''):
         if job == False:
             self.prompt()
         else:
@@ -25,12 +26,13 @@ class Shift():
             self.length = end_time - start_time
             self.date = date
             self.employee = employee
-        job.add_shift(self)
+            self.job = job
+        JOBS[job].add_shift(self)
         EMPLOYEES[employee].append(self)
 
     def prompt(self):
-        start_time = int('0o' + input('Start of shift? in 0700 format'))
-        end_time = int('0o' + input('End of shift? In 1530 format'))
+        start_time = int(input('Start of shift? in 0700 format'))
+        end_time = int(input('End of shift? In 1530 format'))
         date = input('Date of shift?')
         employee = input('Who worked the shift?')
         self.start_time = start_time
@@ -38,12 +40,22 @@ class Shift():
         self.length = end_time - start_time
         self.date = date
         self.employee = employee
+        self.job = self.ask_for_job()
+
+    def ask_for_job(self):
+        while True:
+            job = input('Job number?')
+            if job in JOBS.keys():
+                return job
+
+
 
 
 
 
 def new_job():
-    print('woah a new job')
+    job_num = input('Job number? in format 2295T')
+    Job(job_num)
 
 def new_shift():
  shift = Shift()
